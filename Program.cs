aed70990e2ceaadacde917c1528d51a7d143dcbb
Korobeynikov-Miroslav <63735401+Korobeynikov-Miroslav@users.noreply.github.com>
2023-03-15 10:31:00 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Hello_World
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            byte timer;
+            int money;
+            short helth;
+            long bank;
+            string name;
+            int age;
+            bool currentAge;
+            string greeting;
+            float f;
+            double d;
+
+            timer = 0;
+            money = 100;
+            name = "Aleksandr";
+            helth = 80;
+            bank = 1231546132564612345;
+            age = 36;
+            currentAge = false;
+            greeting = "Welcome! How are you";
+            f = 5.04f;
+            d = 444.123;
+        }
+    }
+}
